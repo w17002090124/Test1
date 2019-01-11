69d47ef8b112c20f3641a11f787013faaeab992a
Administrator <123@qq.com>
2019-01-11 08:33:35 +0800

    1.11(1)

--- session.php
+++ session.php
@@ -6,9 +6,9 @@
     $message=array('message'=>'success');
     if(!isset($_SESSION['name']))
     {
+        
         $message['message']='errors';
         echo json_encode($message);
-        
     }else{
         echo json_encode($message);
     }
